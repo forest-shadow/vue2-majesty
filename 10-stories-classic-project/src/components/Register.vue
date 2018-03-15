c88a40ef60522ef0548bab2f3c3d8084b1602130
forest-shadow <mamasay@yandex.ru>
2018-03-15 17:57:30 +0300

    Add single file component usage example
    * template project generated with vue-cli

---
+++ 10-stories-classic-project/src/components/Register.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="register">
+        <h2>Register Form</h2>
+        <input placeholder="First Name" class="form-control mb-4">
+        <input placeholder="Last Name" class="form-control mb-4">
+        <input placeholder="Email address" class="form-control mb-4">
+        <input placeholder="Pick a password" class="form-control mb-4">
+        <input placeholder="Confirm password" class="form-control mb-4">
+        <button class="btn btn-primary">Sign up</button>
+
+        <famous></famous>
+    </div>
+</template>
+
+<script>
+import Famous from './Famous.vue'
+export default {
+  components: {
+    Famous
+  },
+  created () {
+    console.log('register')
+  }
+}
+</script>
---
+++ 10-stories-classic-project/src/components/Stories.vue
@@ -0,0 +1,49 @@
+<template>
+    <div id="stories">
+        <h2>Stories</h2>
+        <ul class="list-group">
+            <li v-for="story in stories" :key="story.id" class="list-group-item">
+                {{ story.writer }} said "{{ story.plot }}"
+                Story upvotes {{ story.upvotes }}.
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      stories: [
+        {
+          id: 1,
+          plot: 'My horse is amazing.',
+          writer: 'Mr. Weebl',
+          upvotes: 28,
+          voted: false
+        },
+        {
+          id: 2,
+          plot: 'Narwhals invented Shish Kebab.',
+          writer: 'Mr. Weebl',
+          upvotes: 8,
+          voted: false
+        },
+        {
+          id: 3,
+          plot: 'The dark side of the Force is stronger.',
+          writer: 'Darth Vader',
+          upvotes: 52,
+          voted: false
+        },
+        {
+          id: 4,
+          plot: 'One does not simply walk into Mordor',
+          writer: 'Boromir',
+          upvotes: 74,
+          voted: false
+        }
+      ]
+    }
+  }
+}
+</script>
