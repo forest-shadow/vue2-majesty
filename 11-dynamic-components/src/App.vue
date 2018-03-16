7565c70405616ea64598a75df293c31eae868ab3
forest-shadow <mamasay@yandex.ru>
2018-03-16 14:11:40 +0300

    Add dynamic components usage example

---
+++ 11-dynamic-components/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <component v-bind:is="currentComponent"></component>
+    <p>This is very useful...</p>
+    <a href="#" @click="currentComponent = 'hello'">Show Hello</a>
+    <a href="#" @click="currentComponent = 'greet'">Show Greet</a>
+
+    <pre class="mt-4">{{ $data }}</pre>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+import Greet from './components/Greet'
+
+export default {
+  name: 'App',
+  components: {
+    Hello,
+    Greet
+  },
+  data () {
+    return {
+      currentComponent: 'hello'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 12-social-form-tabs/src/components/Login.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="login">
+        <h2>Sign in</h2>
+        <input type="email" class="form-control mb-4" placeholder="Email address">
+        <input type="password" class="form-control mb-4" placeholder="Password">
+        <button class="btn">Sign in</button>
+    </div>
+</template>
+
+<script>
+export default {
+  created () {
+    console.log('login')
+  }
+}
+</script>
